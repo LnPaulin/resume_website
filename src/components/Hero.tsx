ba6608aa662d4632ee8aa26e9c6f52e838fb6a6f
Fonkwa Paulin <fonkwapaulin@gmail.com>
2024-09-02 19:45:28 -0500

    location changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ function Hero() {
               d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
             />
           </svg>
-          <span>Douala, CM</span>
+          <span>Markham, CA</span>
         </motion.div>
         <motion.p
           initial={{ opacity: 0, y: 100 }}
